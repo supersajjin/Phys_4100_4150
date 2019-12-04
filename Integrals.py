78c4bf2b972283b3b03183dfa67a690246e3c20d
Carlos Aguayza <carlosmaguayza@gmail.com>
2019-12-04 00:41:36 -0500

    Updated Lab

--- Integrals.py
+++ Integrals.py
@@ -1,8 +1,6 @@
 
 # # Importing my packages
 
-# In[1]:
-
 
 import numpy as np
 import scipy.constants as cons
@@ -14,7 +12,6 @@ import matplotlib.pyplot as plt
 
 # A) Showed that the total energy per unit area radiated by a black body
 
-# In[26]:
 
 
 
@@ -28,7 +25,7 @@ print(W)
 
 # B)  Write a program to evaluate the integral in this expression.
 
-# In[3]:
+
 
 
 a = 1e-8 #starting point
@@ -41,8 +38,6 @@ s=0
 
 # Used integration by Trig Substitution
 
-# In[4]:
-
 
 def Function(x):
     z=np.tan(x)
@@ -54,8 +49,6 @@ def Function(x):
 
 # Created a for loop and added all the values together
 
-# In[5]:
-
 
 for k in range (1,n):
        s += Function(a+k*h) 
@@ -63,53 +56,54 @@ for k in range (1,n):
 
 # The value after running the for loop
 
-# In[6]:
+
 
 
 s
 
 
-# In[7]:
+
 
 
 Final= h*(0.5*Function(a)+0.5*Function(b)+ s)
 
 
-# In[8]:
+
 
 
 print("This is the integral: ", Final)
 
 
-# In[10]:
+
 
 
 Stefan_Boltzmann_constant= Final*W
 
 
-# In[11]:
+
 
 
 print("The Stefan–Boltzmann constant is {r:1.2}".format(r = Stefan_Boltzmann_constant))
 
 
-# In[12]:
+
 
 
 x = np.linspace(1e-8, np.pi/2)
 
 
-# In[13]:
+
 
 
 z=np.tan(x)
 y = Funny=(z**3)*1/np.cos(x)**2/((np.exp(z)-1))
 
 
-# In[14]:
+
 
 
 f=plt.figure()
 ax=f.add_subplot(1,1,1)
 ax.plot(x,y)
+plt.show()
 
